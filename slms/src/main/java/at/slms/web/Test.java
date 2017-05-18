b257bf220fa04b41613143d49dc988ab9b6a8089
Manuel Fuchs <manuel.fuchs90@gmx.at>
2017-05-18 15:15:47 +0200

    Test Environment

--- slms/src/main/java/at/slms/web/Test.java
+++ slms/src/main/java/at/slms/web/Test.java
@@ -14,9 +14,6 @@ import java.io.Serializable;
 @Stateless
 public class Test {
 
-    @PersistenceContext
-    private EntityManager em;
-
     @GET
     //@Produces(MediaType.APPLICATION_JSON)
     public Response testGetter() {
